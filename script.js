ffd1037467b497b394a5ee83cb235c8fee327e89
CarlosEdson <carlosedsonduarte@gmail.com>
2024-09-22 12:15:35 -0300

    criação de elementos

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-let metas = document.querySelector('#obj')
+let input = document.querySelector('#obj')
+const ul = document.querySelector('ul')
+let metasProgramadas = []
 
 document.querySelector('button').addEventListener("click", () => {
-    resultado.innerHTML += metas.value
-    resultado.innerHTML += ("<br>")
+    let valor = input.value
+
+    const li = document.createElement('li')
+    li.innerHTML = valor
+    ul.appendChild('li')
+
+
+
+    console.log(input.value)
 })
