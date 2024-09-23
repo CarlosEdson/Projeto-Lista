c0aafd174c20dd8ab1373a0bfb84dc0c76dbb9cb
CarlosEdson <carlosedsonduarte@gmail.com>
2024-09-23 13:32:41 -0300

    correção de erros

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-let input = document.querySelector('#obj')
 const ul = document.querySelector('ul')
 let metasProgramadas = []
 
 document.querySelector('button').addEventListener("click", () => {
+    let input = document.querySelector('#obj')
     let valor = input.value
 
     const li = document.createElement('li')
-    li.innerHTML = valor
-    ul.appendChild('li')
+    li.textContent = valor
+    li.setAttribute()
+    ul.appendChild(li);
+
+
+
+    input.textContent = ""
+    input.focus()
+
+
 
 
 
