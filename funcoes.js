2f1fb672c91d5f1fb1b5199d6d9e55d4c5b210ca
CarlosEdson <carlosedsonduarte@gmail.com>
2024-10-19 12:02:35 -0300

    resolução de erros

--- funcoes.js
+++ funcoes.js
@@ -4,53 +4,53 @@ document.querySelector('#adicionar').addEventListener
 ('click', () => {
     let camponome = document.querySelector('#campo-tarefas').value
 
-    if(camponome === "" || camponome === taref){
-        alert("ERRO, CAMPO NÃO PREENCHIDO OU TAREFA REPETIDA")
+    if(camponome === ""){
+        alert("ERRO, CAMPO NÃO PREENCHIDO")
     } else {
         if(taref.includes(camponome)){
             alert("ERRO, JÁ CADASTRADO")
         } else {
             taref.push(camponome)
         }
+
+        function criandoElementos(){
+            let li = document.createElement('li')
+            let ul = document.querySelector('ul')
+            ul.appendChild(li)
+            li.innerHTML += camponome
+    
+            let botaoConfirmar = document.createElement('button')
+            li.appendChild(botaoConfirmar)
+            botaoConfirmar.innerHTML = `<i class="ph-bold ph-check-fat"></i>`
+            botaoConfirmar.style.background = "green";
+            botaoConfirmar.style.color = "black";
+            botaoConfirmar.style.fontSize = "15px";
+            botaoConfirmar.style.cursor = "pointer";
+            botaoConfirmar.style.marginLeft = "340px";
+    
+            botaoConfirmar.addEventListener('click', () => {
+                alert("PARABENS, TAREFA CONCLUIDA")
+                li.style.textDecoration = "line-through";
+    
+                li.removeChild(botaoConfirmar)
+            })
+    
+            let botaoCancelar = document.createElement('button')
+            li.appendChild(botaoCancelar)
+            botaoCancelar.innerHTML = `<i class="ph-bold ph-x"></i>`
+            botaoCancelar.style.background = "red";
+            botaoCancelar.style.color = "black";
+            botaoCancelar.style.fontSize = "15px";
+            botaoCancelar.style.cursor = "pointer";
+    
+            botaoCancelar.addEventListener('click', () => {
+                ul.removeChild(li)
+                taref.pop(ul)
+            })
+        }
+        criandoElementos()
     }
 
-    function criandoElementos(){
-        let li = document.createElement('li')
-        let ul = document.querySelector('ul')
-        ul.appendChild(li)
-        li.innerHTML += camponome
-
-        let botaoConfirmar = document.createElement('button')
-        li.appendChild(botaoConfirmar)
-        botaoConfirmar.innerHTML = `<i class="ph-bold ph-check-fat"></i>`
-        botaoConfirmar.style.background = "green";
-        botaoConfirmar.style.color = "black";
-        botaoConfirmar.style.fontSize = "15px";
-        botaoConfirmar.style.cursor = "pointer";
-        botaoConfirmar.style.marginLeft = "340px";
-
-        botaoConfirmar.addEventListener('click', () => {
-            alert("PARABENS, TAREFA CONCLUIDA")
-            li.style.textDecoration = "line-through";
-
-            li.removeChild(botaoConfirmar)
-        })
-
-        let botaoCancelar = document.createElement('button')
-        li.appendChild(botaoCancelar)
-        botaoCancelar.innerHTML = `<i class="ph-bold ph-x"></i>`
-        botaoCancelar.style.background = "red";
-        botaoCancelar.style.color = "black";
-        botaoCancelar.style.fontSize = "15px";
-        botaoCancelar.style.cursor = "pointer";
-
-        botaoCancelar.addEventListener('click', () => {
-            ul.removeChild(li)
-            taref.pop(ul)
-        })
-    }
-
-    criandoElementos()
 
     console.log(taref)  //campo de acompanhamento
     console.log(camponome) //campo de acompanhamento
