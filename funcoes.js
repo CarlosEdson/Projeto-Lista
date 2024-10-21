f89b47d6c80dba5aa97f10ecae11f01d965467ff
CarlosEdson <carlosedsonduarte@gmail.com>
2024-10-21 08:49:14 -0300

    resolução do problema do input

--- funcoes.js
+++ funcoes.js
@@ -11,47 +11,49 @@ document.querySelector('#adicionar').addEventListener
             alert("ERRO, JÁ CADASTRADO")
         } else {
             taref.push(camponome)
-        }
-
-        function criandoElementos(){
-            let li = document.createElement('li')
-            let ul = document.querySelector('ul')
-            let div = document.createElement('div')
-            div.appendChild(li)
-            li.innerHTML += camponome
-
-            ul.appendChild(div)
-            div.style.display = "flex";
-
-            let checkbox = document.createElement('input:checkbox')
-            div.appendChild(checkbox)
-            checkbox.innerHTML = '<input type="checkbox">'
-            checkbox.style.position = "absolute";
-            checkbox.style.margin = "5px";
-            checkbox.addEventListener("click", () => {
+            
+            function criandoElementos(){
+                let li = document.createElement('li')
+                let ul = document.querySelector('ul')
+                let div = document.createElement('div')
+                div.appendChild(li)
+                li.innerHTML += camponome
+    
+                ul.appendChild(div)
+                div.style.display = "flex";
+    
+                let checkbox = document.createElement('input:checkbox')
+                div.appendChild(checkbox)
+                checkbox.innerHTML = '<input type="checkbox">'
+                checkbox.style.position = "absolute";
+                checkbox.style.margin = "5px";
+                checkbox.addEventListener("click", () => {
                 li.style.textDecoration = "line-through";
                 alert("PARABENS, TAREFA CONCLUIDA")
-            })
-
-            let botaoCancelar = document.createElement('button')
-            div.appendChild(botaoCancelar)
-            botaoCancelar.innerHTML = `<i class="ph-bold ph-x"></i>`
-            botaoCancelar.style.background = "red";
-            botaoCancelar.style.color = "black";
-            botaoCancelar.style.fontSize = "15px";
-            botaoCancelar.style.cursor = "pointer";
-            botaoCancelar.style.height = "20px";
-            botaoCancelar.style.width = "20px";
-            botaoCancelar.style.position = "absolute";
-            botaoCancelar.style.marginLeft = "85%";
-            botaoCancelar.style.marginTop = "3px";
+                taref.pop(div)
+                })
     
-            botaoCancelar.addEventListener('click', () => {
-                ul.removeChild(div)
-                taref.pop(ul)
-            })
+                let botaoCancelar = document.createElement('button')
+                div.appendChild(botaoCancelar)
+                botaoCancelar.innerHTML = `<i class="ph-bold ph-x"></i>`
+                botaoCancelar.style.background = "red";
+                botaoCancelar.style.color = "black";
+                botaoCancelar.style.fontSize = "15px";
+                botaoCancelar.style.cursor = "pointer";
+                botaoCancelar.style.height = "20px";
+                botaoCancelar.style.width = "20px";
+                botaoCancelar.style.position = "absolute";
+                botaoCancelar.style.marginLeft = "85%";
+                botaoCancelar.style.marginTop = "3px";
+        
+                botaoCancelar.addEventListener('click', () => {
+                    ul.removeChild(div)
+                    taref.pop(ul)
+                })
+            }
+            criandoElementos()
         }
-        criandoElementos()
+        document.querySelector('#campo-tarefas').value = "";
     }
     document.querySelector('#campo-tarefas').focus()
 
